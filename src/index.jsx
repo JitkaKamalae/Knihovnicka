ff8e69cd62879ed6e5cc905ece4c70178c83a869
Jitka Ševčíková <jitkakamalae@gmail.com>
2021-05-29 16:36:05 +0200

    obsah

---
+++ src/index.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import { render } from 'react-dom';
+import './style.css';
+
+import Book from './Book';
+import { books } from './books';
+
+const App = () => {
+  return (
+    <>
+      <header className="header">
+        <h1>Moje knihovnička</h1>
+        <div className="icon">
+          <img src="/assets/book.svg" alt="" />
+        </div>
+      </header>
+
+      <div className="library">
+        {books.map((book) => (
+          <Book
+            key={book.id}
+            id={book.id}
+            title={book.title}
+            author={book.author}
+            year={book.year}
+            cover={book.cover}
+            read={book.read}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+render(<App />, document.querySelector('#app'));
